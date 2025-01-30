1488dd6abb6799f991b3b2ab9f161c99336a416e
itselliexo <elliemjoliver@gmail.com>
2025-01-30 15:31:05 +0000

    auto rifle working

--- BuGameJam2025/Assets/Scripts/WeaponSwitch.cs
+++ BuGameJam2025/Assets/Scripts/WeaponSwitch.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class WeaponSwitch : MonoBehaviour
 {
     [SerializeField] private GameObject gun;
+    [SerializeField] private GameObject autoRifle;
     [SerializeField] private GameObject purifier;
     // Start is called before the first frame update
     void Start()
     {
+        if (gun == null)
+        {
+            gun = GameObject.FindGameObjectWithTag("Gun");
+        }
+        if (autoRifle == null)
+        {
+            autoRifle = GameObject.FindGameObjectWithTag("AutoRifle");
+        }
+        if (purifier == null)
+        {
+            purifier = GameObject.FindGameObjectWithTag("Purifier");
+        }
         gun.SetActive(true);
+        autoRifle.SetActive(false);
         purifier.SetActive(false);
     }
 
@@ -20,12 +34,20 @@ public class WeaponSwitch : MonoBehaviour
         {
             gun.SetActive(true);
             purifier.SetActive(false);
+            autoRifle?.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             gun.SetActive(false);
             purifier.SetActive(true);
+            autoRifle.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            gun.SetActive(false);
+            purifier.SetActive(false);
+            autoRifle.SetActive(true);
         }
     }
 }
